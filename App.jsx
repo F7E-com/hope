cd1d4ca4ae4dfcfb80b035ee16b9076502d0b397
f7e-com <faction7entertainment@gmail.com>
2025-08-16 11:54:36 -0700

    Moved index.html layout and content to MainLayout

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import MainLayout from "./midnight-ui/src/layouts/MainLayout";
+import Home from "./midnight-ui/src/pages/Home";
+
+export default f<body>
+
+
+function App() {
+  return (
+    <MainLayout>
+      <Home />
+    </MainLayout>
+  );
+}
--- midnight-ui/src/layouts/mainlayout.jsx
+++ midnight-ui/src/layouts/mainlayout.jsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 export default function MainLayout({ children }) {
+  // State to control which popup is open
+  const [activePopup, setActivePopup] = useState(null);
+
+  const togglePopup = (id) => {
+    setActivePopup(activePopup === id ? null : id);
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
+
+      {/* Background Overlay */}
+      <div className="background"></div>
+
+      {/* Profile Button */}
+      <div
+        className="circle-button profile-button"
+        onClick={() => togglePopup("profile")}
+      ></div>
+      {activePopup === "profile" && (
+        <div id="profilePopup" className="popup">
+          <a href="#">Profile Pic</a>
+          <a href="#">Bio</a>
+          <a href="#">Creator Page</a>
+        </div>
+      )}
+
+      {/* Menu Button */}
+      <div
+        className="circle-button menu-button"
+        onClick={() => togglePopup("menu")}
+      ></div>
+      {activePopup === "menu" && (
+        <div id="menuPopup" className="popup">
+          <a href="#">Watch</a>
+          <a href="#">Read</a>
+          <a href="#">Listen</a>
+          <a href="#">Play</a>
+          <a href="#">Events</a>
+          <a href="#">Search</a>
+          <a href="#">Home</a>
+        </div>
+      )}
+
+      {/* Banner */}
+      <div className="title2">Faction Seven</div>
+
+      {/* Slogan */}
+      <div className="slogan">Your one-stop media source</div>
+
+      {/* Page content gets injected here */}
       <main className="flex-grow container mx-auto p-4">{children}</main>
     </div>
   );
 }
+
