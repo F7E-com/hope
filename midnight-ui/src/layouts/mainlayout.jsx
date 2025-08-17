e0ea97f13684703a639eaa04f81473ace10ea67f
f7e-com <faction7entertainment@gmail.com>
2025-08-16 23:21:34 -0700

    Added ref import

--- midnight-ui/src/layouts/mainlayout.jsx
+++ midnight-ui/src/layouts/mainlayout.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { useState, useEffect, useRef } from "react";
 import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
 
-
 export default function MainLayout({ children }) {
-  // State to control which popup is open
   const [activePopup, setActivePopup] = useState(null);
   const [popupPos, setPopupPos] = useState({ x: 0, y: 0 });
   const popupRef = useRef(null);
@@ -14,16 +11,9 @@ export default function MainLayout({ children }) {
     if (activePopup === id) {
       setActivePopup(null);
     } else {
-      // where the click happened
       const clickX = e.clientX;
       const clickY = e.clientY;
-
-      // temp coords
-      let x = clickX;
-      let y = clickY;
-
-      // check bounds after render in useEffect
-      setPopupPos({ x, y });
+      setPopupPos({ x: clickX, y: clickY });
       setActivePopup(id);
     }
   };
@@ -47,7 +37,6 @@ export default function MainLayout({ children }) {
       const popup = popupRef.current.getBoundingClientRect();
       const viewportWidth = window.innerWidth;
 
-      // if popup goes past right edge, flip left
       if (popup.right > viewportWidth) {
         setPopupPos((pos) => ({ ...pos, x: pos.x - popup.width }));
       }
@@ -57,49 +46,55 @@ export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
-
-      {/* Background Overlay */}
       <div className="background"></div>
 
       {/* Profile Button */}
       <div
         className="circle-button profile-button"
-        onClick={() => togglePopup("profile")}
+        onClick={(e) => togglePopup("profile", e)}
       ></div>
-      {activePopup === "profile" && (
-        <div id="profilePopup" className="popup">
-          <Link to="/new-user">New User/Login</Link>
-          <a href="#">Bio</a>
-          <a href="#">Creator Page</a>
-        </div>
-      )}
 
       {/* Menu Button */}
       <div
         className="circle-button menu-button"
-        onClick={() => togglePopup("menu")}
+        onClick={(e) => togglePopup("menu", e)}
       ></div>
-      {activePopup === "menu" && (
-        <div id="menuPopup" className="popup">
-          <a href="#">Watch</a>
-          <a href="#">Read</a>
-          <a href="#">Listen</a>
-          <a href="#">Play</a>
-          <a href="#">Events</a>
-          <a href="#">Search</a>
-          <a href="#">Home</a>
+
+      {/* Popup */}
+      {activePopup && (
+        <div
+          ref={popupRef}
+          className="popup"
+          style={{
+            top: `${popupPos.y}px`,
+            left: `${popupPos.x}px`,
+            position: "absolute",
+          }}
+        >
+          {activePopup === "profile" && (
+            <>
+              <Link to="/new-user">New User/Login</Link>
+              <a href="#">Bio</a>
+              <a href="#">Creator Page</a>
+            </>
+          )}
+          {activePopup === "menu" && (
+            <>
+              <a href="#">Watch</a>
+              <a href="#">Read</a>
+              <a href="#">Listen</a>
+              <a href="#">Play</a>
+              <a href="#">Events</a>
+              <a href="#">Search</a>
+              <a href="#">Home</a>
+            </>
+          )}
         </div>
       )}
 
-      {/* Banner */}
       <div className="title2">Faction Seven</div>
-
-      {/* Slogan */}
       <div className="slogan">Your one-stop media source</div>
-
-      {/* Page content gets injected here */}
       <main className="flex-grow container mx-auto p-4">{children}</main>
     </div>
   );
 }
-
