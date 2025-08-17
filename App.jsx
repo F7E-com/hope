06f0935d2f79839ee6285d9db63ac9208efa942b
f7e-com <faction7entertainment@gmail.com>
2025-08-17 15:03:24 -0700

    u

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./midnight-ui/src/layouts/MainLayout";
 import Home from "./midnight-ui/src/pages/Home";
 import NewUser from "./midnight-ui/src/pages/NewUser";
+import ActiveProjects from "./midnight-ui/src/pages/ActiveProjects";
 import Profile from "./midnight-ui/src/pages/Profile"; // import your Profile page
 import { UserProvider } from "./midnight-ui/src/contexts/UserContext"; // import the context provider
 
