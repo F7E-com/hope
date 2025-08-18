4b3889c3e672fdef4fd8faeaa352e7907f1e9c39
Faction Seven Entertainment <faction7entertainment@gmail.com>
2025-08-17 18:56:43 -0700

    added theme context

---
+++ midnight-ui/src/contexts/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+      const [currentTheme, setCurrentTheme] = useState("DarkTheme");
+
+        return (
+                <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
+                      {children}
+                          </ThemeContext.Provider>
+        );
+};
+
+export const useTheme = () => useContext(ThemeContext);
+        )
+}
