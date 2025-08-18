26207733215915734fbd1d172f73d4b8116e1e29
f7e-com <faction7entertainment@gmail.com>
2025-08-17 20:14:26 -0700

    u

--- midnight-ui/src/pages/Profile.jsx
+++ midnight-ui/src/pages/Profile.jsx
@@ -5,8 +5,8 @@ import { db } from "@/utils/firebase";
 import BioBox from "../components/BioBox";
 import { useUser } from "../contexts/UserContext";
 import { FACTION_THEMES } from "../themes"; // master theme file
-import ThemePicker from "../components/ThemePicker/ThemePicker";
-import "../Themes/ValeTheme.css"; // import all theme CSS files here
+import ThemePicker from "../components/ThemePicker";
+import "../themes/Vale.css"; // import all theme CSS files here
 
 export default function Profile() {
   const { uid } = useParams();
